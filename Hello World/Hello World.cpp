b02c78e7237865a2e8250d89f99d538507a00193
CCC <carlos.cortes@saei.mx>
2023-07-17 17:31:03 -0600

    Ahora es un codigo que hace algo, no algo muy util pero algo.

--- Hello World/Hello World.cpp
+++ Hello World/Hello World.cpp
@@ -1,11 +1,75 @@
 #include <iostream>
+#include "Windows.h"
+//traducir a humano
+//cadena inmutable
+const char* getProcType(int proc)
+{
+    switch (proc)
+    {
+    case 0: return "x86";
+    case 5: return "ARM";
+    case 6: return "Intel Titanium - based";
+    case 9: return "x64 (AMD or Intel)";
+    case 12: "ARM64";
+    default: return "Unkwon";
+    }
+}
+
+const char* getTitleFromKarma(byte karma) {
+    if (karma >= 0 && karma < 25) return "Hitler";
+    else if (karma >= 25 && karma < 50) return "Despota";
+    else if (karma >= 50 && karma < 100) return "Tirano";
+    else if (karma >= 100 && karma < 150) return "Neutro";
+    else if (karma >= 150 && karma < 200) return "Bondadoso";
+    else if (karma >= 200 && karma < 230) return "Santo";
+    else if (karma >= 230 && karma <= 255) return "Divino";
+    else return "Unknown";
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //pruebas con datos atomicos
+    int i = 2147483647;
+    std::cout << "overflow de int" << i << "\n";
 
-    std::cout << "I did it\n";
+    std::cout<< "i = " << i << "\n";
+    i++;
+    std::cout << "i = " << i << "\n";
 
-    std::cout << "Another one\n";
+    unsigned short int usi = 65635;
+
+    std::cout << "overflow de unsigned short int" << usi << "\n";
+
+    std::cout << "usi = " << usi << "\n";
+    usi++;
+    std::cout << "usi = " << usi << "\n";
+
+    short int si = 20000;
+
+    std::cout << "overflow de signed short int" << si << "\n";
+
+    std::cout << "usi = " << si << "\n";
+    si = si+20000;
+    std::cout << "usi = " << si << "\n";
+
+    //ejemplo del bud de ghandi
+    //definimos el "karma como el nivel de maldad o bondad, va de 0 a 255
+    //karma 0 es malvado
+    //karma 128 es neutral
+    //karma 255 muy bondadoso
+    byte karma = 128;
+    std::cout << "karma: " << (int)karma << "\n";
+
+    for (int x = 0; x < 20; x++) 
+    {
+        karma += 10;
+
+        std::cout << "incrementando karma en 10: " << static_cast<int>(karma) << " - " << getTitleFromKarma(karma) << "\n";
+    }
+
+    SYSTEM_INFO sisinfo;
+    GetSystemInfo(&sisinfo);
+    std::cout << "Procesador: " << getProcType(sisinfo.wProcessorArchitecture) << "\n";
 
     return 0;
 }
